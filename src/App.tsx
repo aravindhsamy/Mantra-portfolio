8b25fa5cfe2db1c6831844c50e13d2e53a0a9751
Aravindhsamy <111353564+aravindhsamy@users.noreply.github.com>
2026-06-22 11:20:45 +0530

    update1.1

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
         <div className="container">
 
           <a className="navbar-brand" href="#">
-            PRISM
+            PRISM-2026
           </a>
 
           <button
